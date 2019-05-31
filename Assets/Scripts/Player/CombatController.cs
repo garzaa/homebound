64678533c9cea9899d806b67d279e7bb1d896849
garzaa <adrianhgarza@gmail.com>
2019-05-30 20:04:27 -0700

    collision sizer

--- Assets/Scripts/Player/CombatController.cs
+++ Assets/Scripts/Player/CombatController.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class CombatController : Entity {
+    Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
